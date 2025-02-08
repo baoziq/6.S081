a79478f190c1e33e91c39dcdd1f61198b4d83196
baozi <caz69012@gamil.com>
2025-02-08 20:15:31 +0800

    pte printout solution

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,30 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+void vm_print(pagetable_t pagetable, int len) {
+  char* str[] = {
+    "..",
+    ".. ..",
+    ".. .. .."
+  };
+  if (len > 2) {
+    printf("error\n");
+    return;
+  }
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if (pte & PTE_V) {
+      printf("%s%d: pte %p pa %p\n", str[len], i, pte, PTE2PA(pte));
+      if ((pte & (PTE_R|PTE_W|PTE_X)) == 0) {
+          uint64 child = PTE2PA(pte);
+          vm_print((pagetable_t)child, len + 1);
+      }
+    }
+  }
+  return ;
+}
+void vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", pagetable);
+  vm_print(pagetable, 0);
+  return;
+}
