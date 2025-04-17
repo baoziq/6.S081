0d59d2c509aa642e1b351866f5e29bab694c74cf
baozi <caz69012@gamil.com>
2025-04-17 13:59:58 +0800

    4.17

--- user/find.c
+++ user/find.c
@@ -1,4 +1,3 @@
-// copy
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
--- user/sleep.c
+++ user/sleep.c
@@ -2,7 +2,6 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-
 int main(int argc, char* argv[]) {
    if (argc != 2) {
     fprintf(2, "usage: sleep 10");
